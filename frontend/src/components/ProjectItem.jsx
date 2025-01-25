23cfa98ec2660e69375c3fbc6af8cd8d37bb66ea
vidalhuix <arnauvidal@gmail.com>
2025-01-25 14:39:05 +0100

    Add backend to the project in order to create a DB for the projects

---
+++ frontend/src/components/ProjectItem.jsx
@@ -0,0 +1,25 @@
+export const ProjectItem = ({
+  name,
+  year,
+  office,
+  client,
+  role,
+  description,
+  link,
+  images,
+
+}) => {
+ 
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-gray-500">
+      <h3 className="absolute text-white bg-black/50 px-2 text-2xl text-shadow font-bold text-center group-hover:hidden">
+        {name.toUpperCase()}
+      </h3>
+     <p>{year}</p>
+     <p>{client}</p>
+     <p>{office}</p>
+     <p>{description}</p>
+     <a>{link}</a>
+    </div>
+  );
+};
